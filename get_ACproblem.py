471833df8391e62c16775140388a5eb6518a46da
umiyuri777 <yuseiai15@gmail.com>
2024-11-08 16:49:55 +0900

    feat: 問題リンクの送信を実装

--- get_ACproblem.py
+++ get_ACproblem.py
@@ -3,26 +3,31 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import time
 from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright  # Playwrightの非同期APIをインポート
+import random
+import asyncio
 
 ACproblems_url = "https://kenkoooo.com/atcoder/#/table/"
 
-def get_ACproblem() -> str:
+async def get_ACproblem() -> list[str]:
 
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch()
-        context = browser.new_context()
+    async with async_playwright() as playwright:
+        browser = await playwright.chromium.launch()
+        context = await browser.new_context()
 
         # ページを開く
-        page = context.new_page()
+        page = await context.new_page()
 
         # 指定したURLに遷移
-        page.goto(ACproblems_url)
+        await page.goto(ACproblems_url)
 
         # javascriptによってページが変わるまで待機
-        time.sleep(3)
+        await asyncio.sleep(3)
+
+        content = await page.content()
 
         # BeautifulSoupでHTMLを解析
-        soup = BeautifulSoup(page.content(), 'html.parser')
+        soup = BeautifulSoup(content, 'html.parser')
 
         trs_with_class = soup.find_all(class_="table-problem")
 
@@ -32,13 +37,6 @@ def get_ACproblem() -> str:
             if temp == None:
                 continue
             problem_url = row.find('a').get('href')
-            print(problem_url)
             problem_links.append(problem_url)
 
-        print(problem_links)
-
-
-
-
-
-get_ACproblem()
+    return random.choice(problem_links)
--- main.py
+++ main.py
@@ -8,10 +8,18 @@ from get_ACproblem import get_ACproblem
 load_dotenv()
 
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-CHANNEL_ID = os.environ['CHANNEL_ID']
+CHANNEL_ID = int(os.environ['CHANNEL_ID'])
 
 # 接続に必要なオブジェクトを追加
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.all())
+
+# Botの起動時に動作する処理
+@client.event
+async def on_ready():
+    print('{0.user}にログインしました。'.format(client))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('!observation'))
+
+    loop.start()
 
 @tasks.loop(seconds=30)
 async def loop():
@@ -21,12 +29,19 @@ async def loop():
     # 現在の時刻を取得
     now = datetime.now().strftime('%H:%M')
 
-    # 現在時刻が9時ならメッセージを送る
-    if now == '09:00':
-        problem_url = get_ACproblem()
+    # # 現在時刻が9時ならメッセージを送る
+    # if now == '09:00':
+    problem_url = await get_ACproblem()
 
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('今日の1問\n', problem_url)
+    print(problem_url)
 
-loop.start()
+
+
+    channel = client.get_channel(CHANNEL_ID)
+    if channel == None:
+        print(f"チャンネルID {CHANNEL_ID} が見つかりませんでした。")
+        return 
+    messege = '今日の1問\n' + problem_url
+    await channel.send(messege)
+
 client.run(ACCESS_TOKEN)
